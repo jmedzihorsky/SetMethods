2100bf6d6a337ebe15c94a05432fa35ac44fd246
nenaoana <nena.oana@yahoo.com>
2016-02-24 13:48:29 +0100

    Update pimdata.R

--- R/pimdata.R
+++ R/pimdata.R
@@ -1,13 +1,16 @@
 pimdata <-
 function(results,
-		 outcome,
-		 solution=1)
-	{
-		s <- results$solution[[solution]]
-		P <- results$pims[colnames(results$pims)%in%s]
+			 outcome, intermed=FALSE,
+			 solution=1)
+	{if (!intermed){
+		  s <- results$solution[[solution]]
+		  P <- results$pims[colnames(results$pims)%in%s]}
+   else{
+      s <- results$i.sol$C1P1$solution[[solution]]
+      P <- results$i.sol$C1P1$IC$overall$pims[colnames(results$i.sol$C1P1$IC$overall$pims)%in%s]}
 		P$solution_formula <- apply(P, 1, max)
 		data <- results$tt$initial.data
-		if (results$options$neg.out) {
+		if (results$opts$neg.out) {
 			P$out <- 1-data[, outcome]
 		} else {
 			P$out <- data[, outcome]
